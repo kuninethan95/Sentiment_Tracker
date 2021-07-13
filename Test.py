37130d04762cd7159b33ed1ad1490b02d5e4d8b6
kuninethan95 <76545496+kuninethan95@users.noreply.github.com>
2021-07-13 07:27:42 -0500

    trying to get pagination

--- Test.py
+++ Test.py
@@ -5,6 +5,8 @@ import pandas as pd
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import datetime
+import SessionState
+
 st.set_page_config(layout="wide")
 
 
@@ -208,5 +210,41 @@ col2.header("Sentiment Distribution")
 col2.plotly_chart(pie_chart)
 
 # st.plotly_chart(pie_chart)
+
+N = 15
+
+st.markdown("# Demonstrating use of Next button with Session State")
+
+# A variable to keep track of which product we are currently displaying
+session_state = st.get(page_number = 0)
+
+data = pd.read_csv('data/etl_df/df_etl_apple1.csv')
+last_page = len(data) // N
+
+# Add a next button and a previous button
+
+prev, _ ,next = st.beta_columns([1, 10, 1])
+
+if next.button("Next"):
+
+    if session_state.page_number + 1 > last_page:
+        session_state.page_number = 0
+    else:
+        session_state.page_number += 1
+
+if prev.button("Previous"):
+
+    if session_state.page_number - 1 < 0:
+        session_state.page_number = last_page
+    else:
+        session_state.page_number -= 1
+
+# Get start and end indices of the next page of the dataframe
+start_idx = session_state.page_number * N 
+end_idx = (1 + session_state.page_number) * N
+
+# Index into the sub dataframe
+sub_df = data.iloc[start_idx:end_idx]
+st.write(sub_df)
 
 
